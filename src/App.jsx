a5ef57aa307d0f1dd7f3c6e0dfd60afd37aaec80
Siddhu Jaykay <siddhujaykay2@gmail.com>
2023-08-13 13:52:11 +0530

    Rewrite

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import "./App.css";
+import Finance from "./components/Finance";
+import Navbar from "./components/Navbar";
+import TodoList from "./components/TodoList";
+
+function App() {
+  return <>
+    {/* <Navbar/> */}
+    <TodoList/>
+    {/* <Finance/> */}
+  </>;
+}
+
+export default App;
