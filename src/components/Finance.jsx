de3d0a9cbf406bf90220497ea36ea46108ba8562
Siddhu Jaykay <siddhujaykay2@gmail.com>
2023-08-15 13:05:19 +0530

    Update Finance.jsx

--- src/components/Finance.jsx
+++ src/components/Finance.jsx
@@ -2,6 +2,56 @@ import axios from "axios";
 import React, { useEffect, useState } from "react";
 import { ToastContainer, toast } from "react-toastify";
 
+let deleteIcon = (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    fill="red"
+    viewBox="0 0 24 24"
+    strokeWidth={1.5}
+    stroke="currentColor"
+    className="w-6 h-6"
+  >
+    <path
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      d="M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 2.077H8.084a2.25 2.25 0 01-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 00-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 013.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 00-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 00-7.5 0"
+    />
+  </svg>
+);
+let IncompleteIcon = (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    fill="yellow"
+    viewBox="0 0 24 24"
+    strokeWidth={1.5}
+    stroke="currentColor"
+    className="w-6 h-6"
+  >
+    <path
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      d="M6 12L3.269 3.126A59.768 59.768 0 0121.485 12 59.77 59.77 0 013.27 20.876L5.999 12zm0 0h7.5"
+    />
+  </svg>
+);
+
+let completeIcon = (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    fill="green"
+    viewBox="0 0 24 24"
+    strokeWidth={1.5}
+    stroke="currentColor"
+    className="w-6 h-6"
+  >
+    <path
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      d="M9 12.75L11.25 15 15 9.75M21 12c0 1.268-.63 2.39-1.593 3.068a3.745 3.745 0 01-1.043 3.296 3.745 3.745 0 01-3.296 1.043A3.745 3.745 0 0112 21c-1.268 0-2.39-.63-3.068-1.593a3.746 3.746 0 01-3.296-1.043 3.745 3.745 0 01-1.043-3.296A3.745 3.745 0 013 12c0-1.268.63-2.39 1.593-3.068a3.745 3.745 0 011.043-3.296 3.746 3.746 0 013.296-1.043A3.746 3.746 0 0112 3c1.268 0 2.39.63 3.068 1.593a3.746 3.746 0 013.296 1.043 3.746 3.746 0 011.043 3.296A3.745 3.745 0 0121 12z"
+    />
+  </svg>
+);
+
 export default function Finance() {
   let [financeItems, setfinanceItems] = useState([]);
 
@@ -11,8 +61,10 @@ export default function Finance() {
     });
   }, []);
   let balance = 0;
+  let owed = 0;
   let financeElements = financeItems.map((item) => {
-    balance += item.amount * (item.mode == "SEND" ? -1 : 1);
+    balance += item.amount * (item.mode == "SEND" ? -1 : 1) * (item.status == "PAID" ? 1 : 0);
+    owed += item.amount * (item.mode == "SEND" ? -1 : 1) * (item.status == "PAID" ? 0 : 1);
     return (
       <FinanceItem
         key={item._id}
@@ -25,11 +77,19 @@ export default function Finance() {
     <div className="h-[90vh] bg-black flex p-5">
       <div className="w-1/4 h-full bg-[#0B0B0B] rounded-xl">
         <div className="text-white text-center text-xl">
-          Balance :
+          Balance : ₹ 
           <span
             className={`${balance < 0 ? "text-red-600" : "text-green-600"}`}
           >
             {balance}
+          </span>
+        </div>
+        <div className="text-white text-center text-xl">
+          Owed : ₹ 
+          <span
+            className={`${owed < 0 ? "text-red-600" : "text-green-600"}`}
+          >
+            {owed}
           </span>
         </div>
         <InputContainer setfinanceItems={setfinanceItems} />
@@ -61,17 +121,25 @@ function FinanceItem({ item, setfinanceItems }) {
       prev.filter((finance) => finance._id != item._id)
     );
   }
+  async function updateStatus(){
+    item.status = item.status == "PAID" ? "UNPAID" : "PAID";
+    let updatedItem = await axios.post(`/api/finance/update`,{
+        _id : item._id,
+        update : {status:item.status}
+    });
+    setfinanceItems((prev)=>[...prev])
+  }
   let isSend = item.mode == "SEND";
   return (
     <div
-      className={`bg-[#141414] p-2 rounded-xl border-b-2 ${
+      className={`bg-[#141414] p-5 rounded-xl border-b-2 ${
         isSend ? "border-b-red-600" : "border-b-green-600"
       }`}
     >
       <div className="flex justify-between">
         <div className="text-white text-xl font-bold">{item.transactee}</div>
         <div className={`${isSend ? "text-red-600" : "text-green-600"}`}>
-          {isSend ? "-" : "+"}${item.amount}
+          {isSend ? "-" : "+"}₹ {item.amount}
         </div>
       </div>
       <div>
@@ -86,22 +154,12 @@ function FinanceItem({ item, setfinanceItems }) {
             onClick={deleteItem}
             className="rounded-full w-5 h-5 hover:scale-125 duration-200 cursor-pointer"
           >
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              fill="red"
-              viewBox="0 0 24 24"
-              strokeWidth={1.5}
-              stroke="currentColor"
-              className="w-6 h-6"
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                d="M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 2.077H8.084a2.25 2.25 0 01-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 00-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 013.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 00-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 00-7.5 0"
-              />
-            </svg>
+            {deleteIcon}
           </div>
-          <div className="rounded-full w-5 h-5 bg-white"></div>
+          <div onClick={updateStatus} 
+          className="rounded-full w-5 h-5 hover:scale-125 duration-200 cursor-pointer">
+            {item.status == "UNPAID" ? IncompleteIcon : completeIcon}
+          </div>
         </div>
       </div>
     </div>
