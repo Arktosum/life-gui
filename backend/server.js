bc9c8fee02157f65d580b88a0da0636723e70b54
Siddhu Jaykay <siddhujaykay2@gmail.com>
2024-03-11 14:08:40 +0530

    fixed environment variables

--- backend/server.js
+++ backend/server.js
@@ -4,12 +4,12 @@ const authRouter = require("./routes/auth"); // Import the router module
 const financeRouter = require("./routes/finance"); // Import the router module
 const friendRouter = require("./routes/friend"); // Import the router module
 const cors = require("cors");
-
+require('dotenv').config();
 const app = express();
-const port = 3000;
+const port = process.env.PORT;
 
 // Connect to MongoDB
-mongoose.connect("mongodb+srv://siddhujaykay2:shiine1984@life-cluster.zopfx6x.mongodb.net/main");
+mongoose.connect(process.env.MONGODB_URI);
 const db = mongoose.connection;
 db.on("error", console.error.bind(console, "MongoDB connection error:"));
 
--- src/app/utils.ts
+++ src/app/utils.ts
@@ -1,2 +1,3 @@
 // export const ORIGIN = 'http://localhost:3000/api'
-export const ORIGIN = 'http://192.168.137.160:3000/api'
+// export const ORIGIN = 'http://192.168.137.160:3000/api'
+export const ORIGIN = 'https://life-gui.onrender.com/api'
